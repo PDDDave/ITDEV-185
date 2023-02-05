0a5fb249b4e74a8996fcc03e438ef320d0348392
David Charles <charled1@gmatc.matc.edu>
2023-02-05 12:10:49 -0600

    Assignment 3 started

---
+++ charles_Assignment3/charles_Assignment3.cpp
@@ -0,0 +1,37 @@
+// charles_Assignment3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int COPPER_PRICE = 100;
+	const int SHIPPING_PRICE = 10;
+	cout << "Wholesale Copper Shipment";
+		
+	do
+	{
+		gatherInfo();
+		shippingReport();
+		again();
+
+	} while (true);
+
+}
+
+void gatherInfo() {
+
+	return;
+}
+
+void shippingReport() {
+
+	return;
+}
+
+void again() {
+	
+	return;
+}
+
